22bbbb653a98e2f0f2fa9551e381828d27cfd7ad
Volodymyr Zavada <volodymyrzavada@gmail.com>
2019-01-18 00:21:59 +0200

    Changed mapping path

--- warehouse-server/src/main/java/app/warehouse/ws/config/WebMvcConfig.java
+++ warehouse-server/src/main/java/app/warehouse/ws/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry
-			.addMapping("/api/**")
+			.addMapping("/**")
 			.allowedOrigins("*")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			// .allowedHeaders("Authorization", "Content-Type")
