e57f50b843c5cb07319d51710a601b842627e12d
Volodymyr Zavada <volodymyrzavada@gmail.com>
2019-01-17 11:42:33 +0200

    Project base setup

---
+++ warehouse-server/src/main/java/app/warehouse/ws/WarehouseServerApplication.java
@@ -0,0 +1,14 @@
+package app.warehouse.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseServerApplication.class, args);
+	}
+
+}
+
