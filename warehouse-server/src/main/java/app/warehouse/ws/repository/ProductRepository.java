d996b6c1cac384163539ffb4208783a8b2892820
Volodymyr Zavada <volodymyrzavada@gmail.com>
2019-01-18 00:22:30 +0200

    Created Product repository with few methods

--- warehouse-server/src/main/java/app/warehouse/ws/repository/ProductRepository.java
+++ warehouse-server/src/main/java/app/warehouse/ws/repository/ProductRepository.java
@@ -1,5 +1,26 @@
 package app.warehouse.ws.repository;
 
-public interface ProductRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import app.warehouse.ws.entity.ProductEntity;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>, JpaSpecificationExecutor<Long> {
+
+	boolean existsByProductCode(String productCode);
+
+	Optional<ProductEntity> findByProductCode(String productCode);
+
+	Optional<ProductEntity> findByProductNameIgnoreCase(String productName);
+
+	List<ProductEntity> findByCategoryId(Long categoryId);
+
+	List<ProductEntity> findByCategoryNameIgnoreCase(String categoryName);
+
+	List<ProductEntity> findByBrandId(Long id);
+
+	List<ProductEntity> findByBrandNameIgnoreCase(String brandName);
 }
