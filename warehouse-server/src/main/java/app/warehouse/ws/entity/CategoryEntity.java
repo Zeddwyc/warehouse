72959d437e00826fb3496d6666d73779008a5a5b
Volodymyr Zavada <volodymyrzavada@gmail.com>
2019-01-18 00:20:54 +0200

    Added entities: Brand, Category, Product

---
+++ warehouse-server/src/main/java/app/warehouse/ws/entity/CategoryEntity.java
@@ -0,0 +1,41 @@
+package app.warehouse.ws.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "categories", indexes = @Index(columnList = "category_name"))
+public class CategoryEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long id;
+
+	@Column(name = "category_name", nullable = false)
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "parent_id", nullable = true)
+	private CategoryEntity parentCategory;
+	
+	@Column(name = "is_archived", columnDefinition = "BOOLEAN DEFAULT FALSE", nullable = false)
+	private boolean isArchived;
+}
---
+++ warehouse-server/src/main/java/app/warehouse/ws/entity/ProductEntity.java
@@ -0,0 +1,60 @@
+package app.warehouse.ws.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Entity
+@Table(name = "products", indexes = @Index(columnList = "product_code, product_name"))
+public class ProductEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long productId;
+
+	@Column(name = "product_code", nullable = false, unique = true)
+	private String productCode;
+
+	@Column(name = "product_name", nullable = false)
+	private String productName;
+
+	@Column(name = "quantity", columnDefinition = "INT DEFAULT 0")
+	private int quantity;
+
+	@Column(name = "cost_price", columnDefinition = "DECIMAL(9,2) DEFAULT 0")
+	private double costPrice;
+
+	@Column(name = "selling_price", columnDefinition = "DECIMAL(9,2) DEFAULT 0")
+	private double sellingPrice;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id", nullable = false)
+	private CategoryEntity category;
+	
+	@ManyToOne
+	@JoinColumn(name = "brand_id", nullable = false)
+	private BrandEntity brand;
+
+	@Column(name = "created_at", updatable = false, nullable = false)
+	private LocalDateTime createdAt = LocalDateTime.now();
+	
+	@Column(name = "is_archived", columnDefinition = "BOOLEAN DEFAULT FALSE", nullable = false)
+	private boolean isArchived;
+}
