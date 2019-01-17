c58c28a2dafa4a56d4fbdffa71479fb5c0d3f2b4
Volodymyr Zavada <volodymyrzavada@gmail.com>
2019-01-18 00:23:11 +0200

    Test random string behavior on the controller layer

--- warehouse-server/src/main/java/app/warehouse/ws/controller/BaseController.java
+++ warehouse-server/src/main/java/app/warehouse/ws/controller/BaseController.java
@@ -2,6 +2,7 @@ package app.warehouse.ws.controller;
 
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.weddini.throttling.Throttling;
 import com.weddini.throttling.ThrottlingType;
 
+import app.warehouse.ws.utils.RandomType;
+import app.warehouse.ws.utils.StringUtils;
+
 @RestController
 @RequestMapping("test")
 public class BaseController {
 
+	@Autowired
+	private StringUtils stringUtils;
+
 	@GetMapping
 	@Throttling(type = ThrottlingType.RemoteAddr, limit = 2, timeUnit = TimeUnit.SECONDS)
 	public String testHello() {
-		System.out.println("Call>");
-		return "hello";
+		return stringUtils.generate(0, RandomType.NUMERIC);
 	}
-	
+
 }
