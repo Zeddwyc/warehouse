46c4c58e2c3a2c741e8149fe8026409fa742c70f
VolodymyrZavada <volodymyrzavada@gmail.com>
2019-01-25 14:42:51 +0200

    Currency parser. Test. Got currencies table

--- warehouse-server/src/main/java/app/warehouse/ws/WarehouseServerApplication.java
+++ warehouse-server/src/main/java/app/warehouse/ws/WarehouseServerApplication.java
@@ -1,5 +1,6 @@
 package app.warehouse.ws;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 import app.warehouse.ws.entity.CurrencyEntity;
 import app.warehouse.ws.repository.CurrencyRepository;
+import app.warehouse.ws.utils.parser.CurrencyParser;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "app.warehouse.ws.repository")
@@ -32,10 +34,15 @@ public class WarehouseServerApplication extends SpringBootServletInitializer {
 	@Autowired
 	private CurrencyRepository currencyRepository;
 	
+	@Autowired
+	private CurrencyParser currencyParser;
+	
 	@Scheduled(cron = "0 */1 * * * *") // Run method every 1 minute
-	public void backgroundTask() {
+	public void backgroundTask() throws IOException {
 		System.out.println("Job start at " + LocalDateTime.now());
 		
+		
+		currencyParser.parse("https://minfin.com.ua/ua/currency/");
 		
 		// Test data - should be replaced by data, parsed from webpage
 		CurrencyEntity currency = new CurrencyEntity();
--- warehouse-server/src/main/java/app/warehouse/ws/utils/parser/CurrencyParser.java
+++ warehouse-server/src/main/java/app/warehouse/ws/utils/parser/CurrencyParser.java
@@ -2,23 +2,50 @@ package app.warehouse.ws.utils.parser;
 
 import java.io.IOException;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CurrencyParser {
 
+	Connection.Response response = null;
+	
 	// https://minfin.com.ua/ua/currency/
-	public void parseCurrencyByUrl(String websiteUrl) {
+	public void parse(String websiteUrl) throws IOException {
+		System.out.println("This");
 		try {
-			Document doc = Jsoup.connect(websiteUrl).get();
+			Connection connection =  Jsoup.connect(websiteUrl);
+			connection.userAgent("Mozilla");
+			connection.timeout(5000);
+			connection.referrer("http://google.com");
 			
-			
-			
+			response = connection.execute();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		test();
+	}
+	
+	private void test() throws IOException {
+		System.out.println("Status code: " + response.statusCode());
+		
+		Document doc = response.parse();
+		
+		Element table = doc.select("table").get(0);
+		Elements tableRows = table.select("tr");
+		
+		for (Element el : tableRows) {
+			Element row = el;
+		    Elements cols = row.select("td");
+		    
+			cols.forEach(System.out::println);
+		}
+		
 	}
 	
 }
